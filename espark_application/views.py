3315aa9fd94ae5e9c5836f2e3867e6bba5de79e5
Mark Martinez <mmartinez8020@gmail.com>
2015-08-23 16:07:44 -0700

    Update views.py

--- espark_application/views.py
+++ espark_application/views.py
@@ -10,15 +10,15 @@ def index(request):
     if request.method == 'POST':
         domain_order = request.FILES['domainorder']
         student_test = request.FILES['studenttests']
-        student_test = create_list(student_test)
-        domain_order = create_list(domain_order)
-        domain_order = create_domain_dict(domain_order)
-        student_list = student_setup(update_values(student_test))
-        final = []
+        student_test_list = create_list(student_test)
+        domain_order_list = create_list(domain_order)
+        domain_order = create_domain_dict(domain_order_list)
+        student_list = student_setup(update_values(student_test_list))
+        student_learning_paths = []
         for student in student_list:
             student_order = create_learning_path(domain_order, student)
-            final.append(student_order)
-        html_table = create_html_table(final)
+            student_learning_paths.append(student_order)
+        html_table = create_html_table(student_learning_paths)
         context = {'myhtml': html_table}
         return JsonResponse(context)
     return render(request, 'index.html')
