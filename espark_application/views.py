5c4f8a2b14b1605e63d2b33730b0a384a3379b3c
Mark Martinez <mmartinez8020@gmail.com>
2015-08-23 16:08:11 -0700

    Update views.py

--- espark_application/views.py
+++ espark_application/views.py
@@ -22,47 +22,5 @@ def index(request):
         context = {'myhtml': html_table}
         return JsonResponse(context)
     return render(request, 'index.html')
-
-
-
-
-
-
-    # domain_order = open(os.path.join(os.path.dirname(__file__), 'csv/domain_order.csv')) 
-    # student_test = open(os.path.join(os.path.dirname(__file__), 'csv/student_tests.csv')) 
-    # domain_test = open(os.path.join(os.path.dirname(__file__), 'csv/domain_order_test.csv'),'rU') 
-    # test_file = open(os.path.join(os.path.dirname(__file__), 'csv/test_file.csv'),'rU') 
-    # student_test = create_list(student_test)
-    # domain_order = create_list(domain_order)
-    # domain_order = create_domain_dict(domain_order)
-    # student_list = student_setup(update_values(student_test))
-    # final = []
-    # for student in student_list:
-    #     student_order = create_learning_path(domain_order, student)
-    #     final.append(student_order)
-    # html_table = create_html_table(final)
-    # context = {'myhtml': html_table}
-    # return render(request, 'index.html', context)
-    
-    
-
-
-
-    # if request.method == 'POST':
-    #     domain_order = request.FILES['domainorder']
-    #     student_test = request.FILES['studenttests']
-    #     student_test = create_list(student_test)
-    #     domain_order = create_list(domain_order)
-    #     domain_order = create_domain_dict(domain_order)
-    #     student_list = student_setup(update_values(student_test))
-    #     final = []
-    #     for student in student_list:
-    #         student_order = real_function(domain_order, student)
-    #         final.append(student_order)
-    #     print final            
-    #     html_table = create_html_table(final)
-    #     context = {'myhtml': html_table}
-    #     print html_table
-    #     return render(request, 'index.html',context)
-    # return render(request, 'index.html')
-    
+  
+  
